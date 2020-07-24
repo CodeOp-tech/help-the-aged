0d8c266e910647671928bebac63a1fefb0c30b52
Albane Chepeau <you@example.com>
2020-07-24 10:18:55 +0100

    HP finished

--- client/src/components/description.js
+++ client/src/components/description.js
@@ -7,7 +7,9 @@ export default class Description extends Component {
   render() {
     return (
       <div>
-        <img src={DESC3} classname="Desc__HH" height="130"/>
+        <br/><br/><br/>
+        <h1>Need help with an elderly loved one?</h1>
+        {/* <img src={DESC3} classname="Desc__T" height="130"/> */}
       <div className="Icons__HP">
         <br/><br/>
         <img src={MISSION} classname="Desc__HH" height="450"/>
